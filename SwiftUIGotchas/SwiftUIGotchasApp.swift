52fc05068a24a80cb5de825b7c0eb40927c37266
Cary Zhou <cary.zhou@doordash.com>
2021-10-10 18:16:45 -0400

    Initial Commit

---
+++ SwiftUIGotchas/SwiftUIGotchasApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGotchasApp.swift
+//  SwiftUIGotchas
+//
+//  Created by Cary Zhou on 10/10/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGotchasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
